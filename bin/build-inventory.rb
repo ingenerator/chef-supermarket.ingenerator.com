a75410d72025136df42a454b35eb8e5a5109520d
Andrew Coulton <andrew@ingenerator.com>
2017-08-10 09:13:15 +0100

    Add crude ability to exclude specific tags from the supermarket
    The postfix-relay cookbook has a tag with the wrong version in the
    metadata. Exclude it from the supermarket because otherwise it
    causes the mirroring to fail with a duplicate cookbook version.

--- bin/build-inventory.rb
+++ bin/build-inventory.rb
@@ -5,6 +5,18 @@ require 'pp'
 require 'octokit'
 require 'yaml'
 
+def parse_definition(yaml_tags)
+  if yaml_tags.is_a? String
+    {
+      'repo' => yaml_tags,
+      'exclude_tags' => []
+    }
+  else
+    yaml_tags
+  end
+end
+
+
 basedir        = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 base_file      = File.join(basedir, 'inventory_base.yml')
 inventory_file = File.join(basedir, 'inventory.yml')
@@ -20,10 +32,18 @@ end
 
 puts "Loading tags for auto-generating repos"
 auto_cookbooks.each do | cookbook_name, cookbook_def |
-    repo = Octokit::Repository.new(cookbook_def['github-tags'])
+    config = parse_definition(cookbook_def['github-tags'])
+    repo   = Octokit::Repository.new(config['repo'])
     puts " - #{cookbook_name} => #{repo.url}"
-    tag_names = Octokit.tags(repo).map{|tag| tag.name}.reverse
-    puts ' -- '+tag_names.join("\n -- ")
+    tag_names     = []
+    Octokit.tags(repo).map{|tag| tag.name}.reverse.each do | tag_name |
+      if config['exclude_tags'].include? tag_name
+        puts " -- !EXCLUDE! #{tag_name}"
+      else
+        puts " -- #{tag_name}"
+        tag_names << tag_name
+      end
+    end
     inventory['cookbooks'][cookbook_name]['git'] = {
       'location' => repo.url,
       'tags' =>tag_names
@@ -33,3 +53,4 @@ end
 puts "Generating compiled #{inventory_file}"
 File.write(inventory_file, "# Auto-compiled from #{base_file}\n"+inventory.to_yaml)
 puts "Inventory file created OK"
+
